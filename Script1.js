ab12b7eb8a206d60cfc29df89f48eb5aad9d88aa
densolo <programmistsolo@gmail.com>
2022-04-19 07:55:01 +0300

    читает текст на наличие слова и возвращает его

--- Script1.js
+++ Script1.js
@@ -1,3 +1,24 @@
 // JavaScript source code
 //XNJ
 //fgh
+var digit = {
+    exec: function (str, pos) {
+        var chr = str.charAt(pos);
+        return { res: chr, end: pos + 1 };
+    }
+};
+console.log(digit.exec("abcde", 0));
+let n = new set();
+var alf = ["à", "á", "â", "ã", "ä", "å"]
+function slovo(str,alb) {
+    let ch;
+    let arr = new String;
+    for (let i = 0; i < str.length - 1; i++) {
+        ch = str[i];
+        for (let j = 0; j < alb.length - 1; j++) {
+            if (ch == alf[j])
+                arr = arr + ch;
+            }
+    }
+    return arr;
+}

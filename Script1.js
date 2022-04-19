6bc2965f5d35f66b449910e38b015aaf4a534671
densolo <programmistsolo@gmail.com>
2022-04-19 18:12:23 +0300

    простейшее считывание слов с ,фиксированным спец знаком, концом

--- Script1.js
+++ Script1.js
@@ -1,24 +1,35 @@
 // JavaScript source code
-//XNJ
-//fgh
-var digit = {
-    exec: function (str, pos) {
-        var chr = str.charAt(pos);
-        return { res: chr, end: pos + 1 };
+
+let n = new Set;
+var alf = ["а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я"]
+var exp = "проверка на содержание[ошиboк]/";
+function simbl(str) {
+    switch (str) {
+        case "[" || "]": return 1;
+        case " ": return 2;
+        case ":": return 3;
+        case "/": return 4;
+        default: return 0;
     }
-};
-console.log(digit.exec("abcde", 0));
-let n = new set();
-var alf = ["а", "б", "в", "г", "д", "е"]
-function slovo(str,alb) {
+}
+
+function slovo(str, alb) {
     let ch;
+    let stop;
     let arr = new String;
-    for (let i = 0; i < str.length - 1; i++) {
+    for (var i = 0; i < str.length; i++) {
+        if (simbl(str[i]) != 0) { exp = str.substr(i + 1, str.length); break; }
         ch = str[i];
-        for (let j = 0; j < alb.length - 1; j++) {
-            if (ch == alf[j])
+        for (let j = 0; j < alb.length; j++) {
+            if (ch == alb[j])
                 arr = arr + ch;
-            }
+        }
     }
+    var cicle = simbl(str[i]);
     return arr;
 }
+while (cicle != 4) {
+    console.log(slovo(exp, alf));
+}
+
+
